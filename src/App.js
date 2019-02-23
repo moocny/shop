b09a6c122957e97bc0b19eab7de984bb4a69a4b8
moocny <p.moocny@gmail.com>
2019-02-23 14:15:30 +0100

    make application layout and menu

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Icon } from 'antd';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        collapsed: false,
+    };
+
+    onCollapse = () =>
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+
+    render() {
+        return (
+            <Layout style={{ minHeight: '100vh' }}>
+                <Layout.Sider
+                    collapsible
+                    collapsed={this.state.collapsed}
+                    onCollapse={this.onCollapse}
+                >
+                    <div
+                        style={{
+                            height: '32px',
+                            margin: '16px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            fontSize: '24px',
+                        }}
+                    >
+                        <Icon
+                            style={{ color: 'white' }}
+                            className="trigger"
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.onCollapse}
+                        />
+                    </div>
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['home', 'categories']}>
+                        <Menu.Item key="home">
+                            <Icon type="home" />
+                            <span>Home</span>
+                        </Menu.Item>
+                        <Menu.SubMenu
+                            key="categories"
+                            title={
+                                <span>
+                                    <Icon type="align-left" />
+                                    <span>Categories</span>
+                                </span>
+                            }
+                        />
+                        <Menu.Item key="account">
+                            <Icon type="user" />
+                            <span>Account</span>
+                        </Menu.Item>
+                        <Menu.Item key="contact">
+                            <Icon type="mail" />
+                            <span>Contact</span>
+                        </Menu.Item>
+                    </Menu>
+                </Layout.Sider>
+                <Layout>
+                    <Layout.Content
+                        style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            background: '#fff',
+                            minHeight: '100%',
+                        }}
+                    >
+                        Content
+                    </Layout.Content>
+                </Layout>
+            </Layout>
+        );
+    }
 }
 
 export default App;
