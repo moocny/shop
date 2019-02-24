35fa4aa2fa5c40c5c44ce66f65410227be0e8741
moocny <p.moocny@gmail.com>
2019-02-24 21:55:19 +0100

    comment home & category

--- src/views/Category.jsx
+++ src/views/Category.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-
-import { ProductCard } from '../components/ProductCard';
 import { Divider } from 'antd';
 
+import { ProductCard } from '../components/ProductCard';
+
+// category
 const CategoryComponent = props => {
+    // find category by slug from url
     const category = props.categories.find(category => category.slug === props.match.params.slug);
+
+    // find products that are in the category by category id
     const products = props.products.filter(product => product.category === category.id);
 
     return (
@@ -20,6 +24,7 @@ const CategoryComponent = props => {
                     flexWrap: 'wrap',
                 }}
             >
+                {/* for each product display product card component */}
                 {products.length > 0
                     ? products.map(product => <ProductCard key={product.id} product={product} />)
                     : 'No products found'}
@@ -28,4 +33,5 @@ const CategoryComponent = props => {
     );
 };
 
+// wrap category with router so it has access to route match params
 export const Category = withRouter(CategoryComponent);
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Carousel, Button } from 'antd';
 
+// function found on internet to get random elements from array
 // https://stackoverflow.com/questions/7158654/how-to-get-random-elements-from-an-array/7158691
-
 function getRandomArrayElements(arr, count) {
     var shuffled = arr.slice(0),
         i = arr.length,
@@ -19,9 +19,11 @@ function getRandomArrayElements(arr, count) {
     return shuffled.slice(min);
 }
 
+// home component
 export const Home = props => {
+
+    // get random 4 products
     const randomProducts = getRandomArrayElements(props.products, 4);
-    console.log(props.categories);
 
     return (
         <div
@@ -33,6 +35,7 @@ export const Home = props => {
                 flexDirection: 'column',
             }}
         >
+            {/* show slider with randomProducts */}
             <div style={{ width: '100%', maxWidth: 400 }}>
                 <Carousel autoplay effect="fade">
                     {randomProducts.map(product => (
@@ -40,10 +43,12 @@ export const Home = props => {
                     ))}
                 </Carousel>
             </div>
+
             <h1 style={{ textTransform: 'uppercase', letterSpacing: 2, margin: '32px 0' }}>
                 Moocny Shop
             </h1>
 
+            {/* show all categories and display them as links to their views */}
             {props.categories.map(category => (
                 <Link
                     key={category.id}
