46fb252ae08b2238243c2aa9134b4bbc3ba970c6
moocny <p.moocny@gmail.com>
2019-02-24 21:47:33 +0100

    comment navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import { Layout, Icon, Menu } from 'antd';
 import { Link } from 'react-router-dom';
 
+// navigation component
 export class Navigation extends Component {
+    // state for menu collapse status
     state = {
         collapsed: false,
     };
 
+    // toggle menu collapsed state
     onCollapse = () =>
         this.setState({
             collapsed: !this.state.collapsed,
         });
 
+    // update collapse menu status on resize
     updateDimensions = () => {
         if (window.innerWidth < 768) {
             this.setState({ collapsed: true });
@@ -20,6 +24,7 @@ export class Navigation extends Component {
         }
     };
 
+    // update collapse menu status on scroll
     updateScroll = () => {
         if (window.scrollY > 200) {
             this.setState({ collapsed: true });
@@ -29,17 +34,14 @@ export class Navigation extends Component {
     };
 
     componentDidMount() {
+        // when component mounts, add resize and scroll event handlers
         window.addEventListener('resize', this.updateDimensions);
         window.addEventListener('scroll', this.updateScroll);
 
+        // when mobile view, show collapsed menu from beginning
         if (window.innerWidth < 768) {
             this.setState({ collapsed: true });
         }
-    }
-
-    componenWillUnmount() {
-        window.removeEventListener('resize', this.updateDimensions);
-        window.removeEventListener('scroll', this.updateScroll);
     }
 
     render() {
@@ -47,6 +49,8 @@ export class Navigation extends Component {
 
         return (
             <Layout.Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+
+                {/* menu toggle button */}
                 <div
                     style={{
                         height: '32px',
@@ -63,7 +67,11 @@ export class Navigation extends Component {
                         onClick={this.onCollapse}
                     />
                 </div>
+
+                {/* menu */}
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['home']}>
+
+                    {/* cart with cart items count */}
                     {cartLength > 0 && (
                         <Menu.Item key="cart">
                             <Link to="/cart">
@@ -73,12 +81,15 @@ export class Navigation extends Component {
                         </Menu.Item>
                     )}
 
+                    {/* home */}
                     <Menu.Item key="home">
                         <Link to="/">
                             <Icon type="home" />
                             <span>Home</span>
                         </Link>
                     </Menu.Item>
+
+                    {/* categories  */}
                     <Menu.SubMenu
                         key="categories"
                         title={
@@ -88,18 +99,23 @@ export class Navigation extends Component {
                             </span>
                         }
                     >
+                        {/* show each of the category as a sub menu item */}
                         {this.props.categories.map(category => (
                             <Menu.Item key={category.id}>
+                                {/* link each menu category item to category url  */}
                                 <Link to={`/category/${category.slug}`}>{category.name}</Link>
                             </Menu.Item>
                         ))}
                     </Menu.SubMenu>
+
+                    {/* account  */}
                     <Menu.Item key="account">
                         <Link to="/account">
                             <Icon type="user" />
                             <span>Account</span>
                         </Link>
                     </Menu.Item>
+
                 </Menu>
             </Layout.Sider>
         );
