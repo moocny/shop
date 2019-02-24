8b30406a0ef5a2e4d121baf0bac116ec1b888987
moocny <p.moocny@gmail.com>
2019-02-24 20:26:17 +0100

    display random product images in home slider

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
-import { Carousel } from 'antd';
+import { Link } from 'react-router-dom';
+import { Carousel, Tag } from 'antd';
 
-export const Home = () => (
-    <div style={{ margin: '-24px' }}>
-        <Carousel autoplay>
-            <div>
-                <h3>1</h3>
-            </div>
-            <div>
-                <h3>2</h3>
-            </div>
-            <div>
-                <h3>3</h3>
-            </div>
-            <div>
-                <h3>4</h3>
-            </div>
-        </Carousel>
-    </div>
-);
+// https://stackoverflow.com/questions/7158654/how-to-get-random-elements-from-an-array/7158691
+
+function getRandomArrayElements(arr, count) {
+    var shuffled = arr.slice(0),
+        i = arr.length,
+        min = i - count,
+        temp,
+        index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+}
+
+export const Home = props => {
+    const randomProducts = getRandomArrayElements(props.products, 4);
+    console.log(props.categories);
+
+    return (
+        <div style={{ margin: '-24px' }}>
+            <Carousel autoplay>
+                {randomProducts.map(product => (
+                    <img src={product.img} key={product.id} alt={product} />
+                ))}
+            </Carousel>
+            {props.categories.map(category => (
+                <Link key={category.id} to={`/category/${category.slug}`}>
+                    <Tag>{category.name}</Tag>
+                </Link>
+            ))}
+        </div>
+    );
+};
