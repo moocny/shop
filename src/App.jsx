8b304fdb0115deed24ed61cb7f0f54c2e740733e
moocny <p.moocny@gmail.com>
2019-02-24 21:40:24 +0100

    comment app

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { Layout } from 'antd';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+// json data
 import categories from './categories.json';
 import products from './products.json';
 
+// views
 import { Home } from './views/Home';
 import { Category } from './views/Category';
 import { Product } from './views/Product';
@@ -13,46 +15,69 @@ import { LogIn } from './views/LogIn.jsx';
 import { SignUp } from './views/SignUp.jsx';
 import { NotFound } from './views/NotFound';
 
+// components
 import { Navigation } from './components/Navigation';
 import { PropsRoute } from './components/PropsRoute.jsx';
 
+// class for main component
 export class App extends Component {
+    // main app state
     state = {
-        categories,
-        products,
-        cart: [],
+        categories, // loads categories data from categories.json
+        products, // loads products data from categories.json
+        cart: [], // will hold products data in the cart
     };
 
-    addToCart = (id, quantity) => {
-        const productInCart = this.state.cart.find(product => product.id === id);
+    // adds product to card by product id and quantity
+    addToCart = (productId, quantity) => {
+        // check if product is already in cart
+        const productInCart = this.state.cart.find(product => product.id === productId);
 
         if (productInCart) {
+            // if yes, find it and update quantity
             const updatedCart = this.state.cart.map(product => {
-                if (product.id === id) {
+                // find the product in cart array
+                if (product.id === productId) {
+                    // update its quantity
                     return { ...product, quantity: product.quantity + quantity };
                 }
 
+                // leave other products as they are
                 return product;
             });
 
+            // set updated cart state
             this.setState({ cart: updatedCart });
         } else {
+            // if no product with id is in cart, add new
+
+            // make new product object
             const newProduct = {
-                id,
+                id: productId,
                 quantity,
             };
 
+            // set state with old cart products and new product
             this.setState({ cart: [...this.state.cart, newProduct] });
         }
     };
 
-    removeFromCart = id =>
-        this.setState({ cart: this.state.cart.filter(product => product.id !== id) });
+    // remove item from cart by id
+    removeFromCart = id => {
+        // filter cart array elements that match id
+        const updatedCart = this.state.cart.filter(product => product.id !== id);
+
+        // set cart state with updated cart
+        this.setState({ cart: updatedCart });
+    };
 
     render() {
         return (
             <BrowserRouter>
+            {/* wrap app with browser router */}
                 <Layout style={{ minHeight: '100vh' }}>
+
+                    {/* pass categories and cart to navigation */}
                     <Navigation categories={this.state.categories} cart={this.state.cart} />
                     <Layout>
                         <Layout.Content
@@ -62,7 +87,11 @@ export class App extends Component {
                                 background: '#fff',
                             }}
                         >
+
+                            {/* switch shows the view based by route */}
                             <Switch>
+
+                                {/* home */}
                                 <PropsRoute
                                     exact
                                     path="/"
@@ -70,6 +99,8 @@ export class App extends Component {
                                     products={this.state.products}
                                     categories={this.state.categories}
                                 />
+
+                                {/* category */}
                                 <PropsRoute
                                     exact
                                     path="/category/:slug"
@@ -78,6 +109,8 @@ export class App extends Component {
                                     products={this.state.products}
                                     images={this.state.images}
                                 />
+
+                                {/* product */}
                                 <PropsRoute
                                     exact
                                     path="/product/:slug"
@@ -87,6 +120,8 @@ export class App extends Component {
                                     images={this.state.images}
                                     addToCart={this.addToCart}
                                 />
+
+                                {/* cart */}
                                 <PropsRoute
                                     exact
                                     path="/cart"
@@ -96,9 +131,16 @@ export class App extends Component {
                                     cart={this.state.cart}
                                     removeFromCart={this.removeFromCart}
                                 />
+
+                                {/* account */}
                                 <Route exact path="/account" component={LogIn} />
+
+                                {/* signup */}
                                 <Route exact path="/signup" component={SignUp} />
+
+                                {/* not found fallback */}
                                 <Route exact component={NotFound} />
+                                
                             </Switch>
                         </Layout.Content>
                     </Layout>
