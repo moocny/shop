ef29efa11a4aaf388977708aba15d0e9a1b88ee5
moocny <p.moocny@gmail.com>
2019-02-24 21:41:35 +0100

    comment index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import ant design css
 import 'antd/dist/antd.css';
+
+// import custom css
 import './index.css';
 
 import { App } from './App';
 
+// render app component in html element with id root
 ReactDOM.render(<App />, document.getElementById('root'));
