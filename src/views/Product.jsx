7225c38da35f566150a1630668cf59971dca6ede
moocny <p.moocny@gmail.com>
2019-02-24 18:55:45 +0100

    update product view

--- src/views/Product.jsx
+++ src/views/Product.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { Button, InputNumber } from 'antd';
 
-export const ProductComponent = props => {
-    const product = props.products.find(product => product.slug === props.match.params.slug);
+export class ProductComponent extends Component {
+    render() {
+        const product = this.props.products.find(
+            product => product.slug === this.props.match.params.slug
+        );
 
-    return <div>{product.name}</div>;
-};
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                }}
+            >
+                <img
+                    style={{ width: '100%', maxWidth: 800 }}
+                    src={product.img}
+                    alt={product.name}
+                />
+                <h1 style={{ margin: '24px 0' }}>{product.name}</h1>
+                <h3>{product.description}</h3>
+                <h3>{product.price}</h3>
+                <div>
+                    <InputNumber placeholder="Quantity" defaultValue={1} />
+                    <Button type="primary">Add to cart</Button>
+                </div>
+            </div>
+        );
+    }
+}
 
 export const Product = withRouter(ProductComponent);
