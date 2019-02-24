213d0c89fec59f0334d9d512fe5bbc304a9df070
moocny <p.moocny@gmail.com>
2019-02-24 23:10:09 +0100

    add gh-pages

---
+++ build/precache-manifest.40e33343811a6dcdf3ee1c9692d683e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9bad1023ca706ffec51",
+    "url": "/shop/static/css/main.65dfe073.chunk.css"
+  },
+  {
+    "revision": "c9bad1023ca706ffec51",
+    "url": "/shop/static/js/main.c9bad102.chunk.js"
+  },
+  {
+    "revision": "463990120ab1f51f2839",
+    "url": "/shop/static/js/runtime~main.46399012.js"
+  },
+  {
+    "revision": "9af8dffe1b714607f044",
+    "url": "/shop/static/css/2.a7f2eda7.chunk.css"
+  },
+  {
+    "revision": "9af8dffe1b714607f044",
+    "url": "/shop/static/js/2.9af8dffe.chunk.js"
+  },
+  {
+    "revision": "e76560daf3d734550a37f69321b37f96",
+    "url": "/shop/index.html"
+  }
+];
