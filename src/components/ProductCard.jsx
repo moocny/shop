8fca94d4eccbb4edff351f34ca0b028e8d7c7f35
moocny <p.moocny@gmail.com>
2019-02-24 21:48:32 +0100

    comment productcard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Divider } from 'antd';
 
+// product card that is displayed on category view
 export const ProductCard = ({ product }) => (
+    // link each product to his view
     <Link to={`/product/${product.slug}`}>
         <Card
             style={{ width: 300, margin: 10 }}
@@ -12,6 +14,7 @@ export const ProductCard = ({ product }) => (
                 <strong style={{ fontSize: 18 }}>{product.name}</strong>
                 <Divider />
                 <h2 style={{ color: 'gray', fontSize: 16 }}>
+                    {/* show product price with fixed comma */}
                     {parseFloat(product.price).toFixed(2)} zł
                 </h2>
                 <p>{product.description}</p>
